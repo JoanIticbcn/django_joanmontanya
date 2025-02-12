465a5e247dd76d60a3e0b1b09b3ff502b60fd2da
Joan <joan@gmail.com>
2025-02-12 19:54:17 +0100

    Correccions

--- activitat14/TIC_BCN/login/views.py
+++ activitat14/TIC_BCN/login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from future.backports.http.client import HTTPResponse
 from .models import Usuari
 from .forms import EmailAuthenticationForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -19,9 +20,10 @@ def ferlogin(request):
         if usuari.email==email and usuari.password == password:
             request.session["email"] = email
             request.session["password"] = password
-            return HTTPResponse("Login correcte")
+            request.session["ID"] = usuari.id_usuari
+            return redirect("inici")
 
-    return HTTPResponse("El usuari no existeix a la Base de dades")
+    return HTTPResponse("Error: El usuari no existeix a la Base de dades o les credencials son erroneas")
 
 def recuperarSessio(request):
     email = request.session.get("email")
@@ -29,16 +31,14 @@ def recuperarSessio(request):
     usuaris = Usuari.objects.all()
     for usuari in usuaris:
         if usuari.email==email and usuari.password == password:
-            return render(request,"inicial.html",{'email':email,'password':password})
+            return render(request,"inicial.html",{'email':email,'password':password,'nom':usuari.nom})
 
-    return HTTPResponse("No tens la sessio iniciada")
+    return HTTPResponse("Error: No tens la sessio iniciada o les credenciasl son erroneas")
 
 def logout(request):
-    request.session["email"] = ""
-    request.session["password"]=""
-    authentication_form = EmailAuthenticationForm()
-    context = {'form': authentication_form}
-    return render(request, "loginform.html",context)
+    request.session.flush()
+    request.session.delete()
+    return redirect('formulari')
 
 def guardarusuari():
     usuari = Usuari(
